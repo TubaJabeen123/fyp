c90edc4d65c99c8d864fbfb4ec866a9c9a9fc4eb
Tuba Jabeen <fa21-bcs-033@cuiatk.edu.pk>
2025-05-18 10:57:39 +0500

    .

--- config/multer-grid-fs.js
+++ config/multer-grid-fs.js
@@ -7,7 +7,7 @@ let gfs;
 let storage;
 
 // MongoDB Connection
-connectToMongoBb("mongodb://127.0.0.1:27017/fyp")
+connectToMongoBb(process.env.MONGODB_URI)
   .then((conn) => {
     console.log("MongoDB connected");
 
